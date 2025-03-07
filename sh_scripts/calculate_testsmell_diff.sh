e3caa0daac9858141ccf1f5a7f1c1ff457f39739
kosei Horikawa <63036066+Mont9165@users.noreply.github.com>
2025-03-07 19:08:14 +0900

    Add shell scripts for cluster

---
+++ sh_scripts/calculate_testsmell_diff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=test-smell
+#SBATCH --output=logs/job.out
+#SBATCH --error=errors/job.err
+#SBATCH --time=4-04:00:00
+#SBATCH --partition=ocigpu8a100_long
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16G
+
+
+module load singularity
+singularity exec collect-test-smell_latest.sif sh /work/kosei-ho/InvestigatingTheImpactOfTestSpecificRefactoring/5_analyze_test_smell/sh/testsmell_diff.sh
+
